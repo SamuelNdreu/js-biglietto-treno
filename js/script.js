c47bb50add755ed629295ca7bab29cbf2b42ff7a
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-21 15:25:36 +0100

    created passenger section

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const numberofKm = parseFloat(prompt("Quanti chilometri devi percorrere?"));
+const passengerAge = parseInt(prompt("Quanti anni hai?"));
+
+document.getElementById('km').innerHTML = numberofKm + " Km";
+document.getElementById('età').innerHTML = passengerAge + " anni";
+
+
